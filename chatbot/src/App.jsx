ae491eb0e4397dd009448345ed5cbd3b43f18602
Gopal Gavas <gopalgavas7@gmail.com>
2025-08-18 21:17:04 +0530

    add new responses for chatbot

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatMessages from "./components/ChatMessages";
 import ChatInput from "./components/ChatInput";
+import { Chatbot } from "supersimpledev";
 import "./App.css";
 
 const App = () => {
   const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      "give me a four digit otp": function () {
+        return `Sure here is your four digit otp: ${Math.floor(
+          Math.random() * 9000 + 1000
+        )}`;
+      },
+      "give me an unqiue Id": function () {
+        return `Here is your unique Id ${crypto.randomUUID()}`;
+      },
+    });
+  });
 
   return (
     <div className="app-container">
