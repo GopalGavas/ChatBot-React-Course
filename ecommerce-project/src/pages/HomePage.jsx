ce75ea67e2e4660dc029b2a40a587c5727695cee
Gopal Gavas <gopalgavas7@gmail.com>
2025-08-25 12:36:23 +0530

    fetch products from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import "./HomePage.css";
 import checkmarkIcon from "../assets/images/icons/checkmark.png";
-import { products } from "../../starting-code/data/products";
+import "./HomePage.css";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <link rel="icon" href="images/home-favicon.png" />
